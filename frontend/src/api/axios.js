884194b1b60cfaf209a01e1653bb94e15c459b32
nikhilarbune <nikhil@example.com>
2026-05-27 00:23:23 +0530

    fixed backend api

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://online-doctor-appointment-system-2go1.onrender.com",
+    baseURL: "https://online-doctor-appointment-system-2go1.onrender.com/api",
 });
 
 export default api;
