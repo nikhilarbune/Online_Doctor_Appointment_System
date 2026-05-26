75175b439602f39b1ac1d1d2798e3017cb411213
nikhilarbune <nikhil@example.com>
2026-05-26 14:37:09 +0530

    final deployment fix

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: `${import.meta.env.VITE_API_URL}/api`,
-    headers: { 'Content-Type': 'application/json' }
+    baseURL: "https://online-doctor-appointment-system-2go1.onrender.com",
 });
 
 export default api;
