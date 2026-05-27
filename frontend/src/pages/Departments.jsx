aba73c7c5cd1aa741abd1e31e628859b751ecaee
Nikhil Arbune <nikhilarbune11@gmail.com>
2026-05-27 21:41:14 +0530

    Update Departments.jsx

--- frontend/src/pages/Departments.jsx
+++ frontend/src/pages/Departments.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import api from '../api/axios.jsx';
+import api from '../api/axios';
 
 function Departments() {
   const [departments, setDepartments] = useState([]);
