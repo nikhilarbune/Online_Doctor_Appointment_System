9206d61c8b7bebb3e1d97fa104c54055692580ac
Nikhil Arbune <nikhilarbune11@gmail.com>
2026-05-27 21:43:20 +0530

    Update Services.jsx

--- frontend/src/pages/Services.jsx
+++ frontend/src/pages/Services.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import api from '../api/axios.jsx';
+import api from '../api/axios';
 
 function Services() {
   const [services, setServices] = useState([]);
